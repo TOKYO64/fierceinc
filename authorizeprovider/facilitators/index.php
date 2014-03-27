a0f28583cc68a9e7dbebca35deca61df1e0bb597
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 10:07:08 -0700

    Create index.php

---
+++ authorizeprovider/facilitators/index.php
@@ -0,0 +1,28 @@
+<?php 
+	$pageTitle = "Facilitators";
+	$tab = "facilitators";
+	include('../includes/head.php');
+?>
+
+	<div id="top_image">
+		<img src="<?php echo $URLROOT; ?>images/app_certified.jpg" width="780" height="214" />
+	</div>
+		
+	<div id="sectionNav">
+		<ul>
+			<li class="active" id="first"><a href="index.php">Overview</a></li>
+			<li><a href="qualifications.php">Qualifications</a></li>
+			<li><a href="process.php">The Process</a></li>
+		</ul>
+	</div>
+		
+	<div id="content">
+		<h1>Certified to spread (and teach) the skill set</h1>
+		<p>At Fierce, we often say that &ldquo;the most valuable thing any of us can do is find a way to say the things that can't be 
+			said.&rdquo; Our Certified Facilitators are individuals who have been certified in just that skill, and more &mdash; 
+			they are people who have successfully completed the Fierce Train-the-Trainer program, and are now certified to lead 
+			Fierce training sessions.</p>
+		<p>Click <a href="<?php echo $URLROOT; ?>facilitators/qualifications.php">here</a> to learn how to qualify for this program.</p>
+	</div>
+		
+<?php include('../includes/foot.php'); ?>
