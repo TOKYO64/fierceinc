13f8a8be7e365b358ec3b96b43108dd21f1bd310
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:39:25 -0700

    Create foot.php

---
+++ includes/foot.php
@@ -0,0 +1,13 @@
+
+		<div id="footer">
+			<div class="links">
+				<a target="FierceInc" href="http://www.fierceinc.com/index.php?page=contact_us">Contact Us</a> | <a target="FierceInc" href="http://www.fierceinc.com/index.php?page=about">About Fierce</a>
+			</div>
+			<p class="colorPrimary">&copy; Copyright 2009. Fierce, Inc. All rights reserved.</p>
+		</div>
+		
+	</div>
+	<div id="borderBot"></div>
+</div>
+</body>
+</html> 
