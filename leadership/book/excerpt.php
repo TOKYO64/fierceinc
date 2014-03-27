65d61cbd2d5f5339240f2b73ec61cfa6114ea832
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:48:51 -0700

    Create excerpt.php

---
+++ leadership/book/excerpt.php
@@ -0,0 +1,43 @@
+<? $pageTitle = 'Excerpt | The Book';
+include('../includes/head.php'); ?>
+
+		
+		<div id="feature" style="background: #e2eef8 url('../images/featBookB.png') no-repeat top left;">
+			<div id="featRt">
+				<img class="upper" src="../images/featRtRes2.png" alt="Free Resources" />
+				<center><a href="<?php echo $URLROOT; ?>resources/"><img class="button" src="../images/butResources.gif" /></a></center>
+			</div>
+		</div>
+		
+		<div id="sectionNav">
+			<ul>
+				<li id="first"><a href="<?php echo $URLROOT; ?>book/">Overview</a></li>
+				<li class="active"><a href="<?php echo $URLROOT; ?>book/excerpt.php">Excerpt</a></li>
+				<li><a href="<?php echo $URLROOT; ?>book/reviews.php">Reviews</a></li>
+			</ul>
+		</div>
+		
+		<div id="content">
+			<h1 class="colorPrimary">a brief excerpt</h1>
+			
+			<p>A tongue-in-cheek website called “MBA jargon watch” (described as “where jargon goes to die”) writes:</p>
+			
+			<br />
+			<h4 class="colorSecondary"><em>“A widely used term, ‘best practices’ is used to describe the ‘best’ techniques or methods in use in a company, field or industry. Unfortunately, companies often confuse latest or trendiest with best, and the best practices of one era are soon superseded by the ever-more-ludicrous fads of the next.”</em></h4>
+			<br />
+
+			<p>At best, these “best” practices are ineffective. At worst, they are costing companies billions of dollars. Yet, no one questions them. </p>
+			
+			<div id="butBuyBook">
+				<a href="<?php echo $URLROOT; ?>buy/"><img src="../images/butBuyBook.gif" alt="BUY THE BOOK" /></a>
+			</div>
+			
+			<p>The goal of this book is to do just that. To help individuals and organizations everywhere abandon those “best” practices that no longer (and perhaps never did) serve us and replace them with superior practices that get the job done. To teach companies and their leaders how to implement these better practices, and as a result, differentiate themselves from everyone else out there, win the promotions and the clients and the market share they want, attract the most talented employees and ideal customers&mdash;and wouldn’t it be nice if the stock price went up instead of down? </p>
+			
+			<p>Here’s some really, really good news. You can do this. Whether you own a small business, work for a multinational corporation, or are employed by a school, non-profit, or the government, the antidotes to the worst “best” practices are at your fingertips&mdash;and they start with you. In essence, they require that you simply abandon costly practices and initiatives that aren’t working and practice something different. Even a subtle shift in one individual’s behavior can set large-scale transformation in progress. I’ve seen it time and time again.</p>
+			
+			<p>It may help to imagine a kaleidoscope. When one piece shifts, the entire picture changes instantly, dramatically. Likewise, when we spot a “tell,” or grasp a fundamental new truth (at least, new to us), we see things entirely differently and we can never go back to the original picture, even if we want to because we can’t un-know what we now know. This book will help you give your kaleidoscope a turn so that you see more, see differently, and given what you see at any point in time, be compelled to act.</p></p>
+
+		</div>
+		
+<? include('../includes/foot.php'); ?>
