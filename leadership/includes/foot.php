2c36b2830ea9fc8b5b3d87764b23c85f28a9e8d9
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:47:42 -0700

    Create foot.php

---
+++ leadership/includes/foot.php
@@ -0,0 +1,28 @@
+		<? if (!$pdfOut) { ?>
+		
+			<div id="footer">
+				<div class="links">
+					<a target="FierceInc" href="http://www.fierceinc.com/index.php?page=contact_us">Contact Us</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a target="FierceInc" href="http://www.fierceinc.com/index.php?page=about">About Fierce</a>
+				</div>
+				<p class="colorPrimary">&copy; Copyright 2009 - <?php echo date('Y'); ?>. Fierce, Inc. All rights reserved.</p>
+			</div>
+		
+		<? } ?>
+		
+	</div>
+	<? if (!$pdfOut) { ?>
+	<div id="borderBot"></div>
+	<? } ?>
+</div>
+
+<!-- Begin Optify Tracking Code -->
+<script type="text/javascript">
+var optJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + optJsHost + 
+"service.optify.net/opt.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript"> try { _opt.view("2HTM8RB6"); } catch(err) {} </script>
+<!-- End Optify Tracking Code -->
+
+</body>
+</html> 
