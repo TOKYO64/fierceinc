2574ecbd42ca2a28cc01a64bfb4ef4b655d14e8b
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:29:26 -0700

    Create index.php

---
+++ bb/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+	<title>Fierce Community</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+ * {margin: 0; padding: 0;}
+
+ h1, h2, h3 {padding: 16px 0 6px 0;}
+
+ h1 {font-size:11pt;color:#EF7827;font-weight:normal;}
+ h2 {font-size:9pt;color:#EF7827;font-weight:bold;}
+ h3 {font-size:9pt;color:#575657;font-weight:bold;}
+
+ img, img:link, a img {border: 0;}
+ ul, ol	{list-style-position: outside; padding-left: 20px;}
+ li {padding: 5px;}
+a {
+	text-decoration: none;
+	color: #005294;
+}
+
+a:hover {
+	text-decoration: none;
+	color: #EA802D;
+}
+</style>
+</head>
+<body style="background:#FFFFFF;color:#575657;font-family:arial,helvetica,sans-serif;font-size:9pt;line-height:18px;">
+<div align="center"><br />
+<table width="598" border="0" padding="0" cellspacing="0">
+
+<tr>
+ <td colspan="3"><img src="http://www.fierceinc.com/uploads/images/special_email/20090413_fierce_email_banner_G.jpg" border="0" /></td>
+</tr>
+
+<tr>
+  <td width="17" background="http://www.fierceinc.com/uploads/images/special_email/special_left.gif">&nbsp;</td>
+  <td width="557" style="color:#575657;font-family:arial,helvetica,sans-serif;font-size:9pt;line-height:18px;">
+<br />
+	<p>Hello Fierce Graduates and Facilitators,</p><br/>
+	<p>Fierce is very pleased to announce and welcome you to join our online 
+	<a href="http://www.fierceinc.com/index.php?page=myfierce">My Fierce</a> community &mdash; a new and improved 
+	community website for all Fierce graduates and facilitators.
+	<a href="http://www.fierceinc.com/index.php?page=myfierce">My Fierce</a> combines the friendly and personal features 
+	of Facebook with the practical features of a resource sharing site.</p><br/>
+	<p>Just like the Fierce Community, this website will serve as a common area to share resources, information, and communication 
+	with one another. We have already posted some amazing resources and tools for sustaining Fierce within your organization. Some of 
+	these resources, tools, and features include:</p><br/>
+	<ul>
+		<li>Conversation posts, made easy: Every user has a "status update bar" to start a conversation, ask a question, and post an idea or comment on others' posts.</li>
+		<li>Fierce Forms: To help with sustaining your Fierce practices, you'll find the forms for each of the conversational models here.</li>
+		<li>Event and News feed: This feature will show new Fierce blog posts, new events and will call out new resources as they are added.</li>
+	</ul><br/>
+	<p>We hope you check those out!</p><br/>
+	<p>We will not be updating the Fierce Community, so please register for 
+	<a href="http://www.fierceinc.com/index.php?page=myfierce">My Fierce</a> if you would like to download updated handouts and 
+	materials. To register for <a href="http://www.fierceinc.com/index.php?page=myfierce">My Fierce</a>, you will need to create a 
+	new username and password, and you will also need to provide a Fierce ID code. To receive your Fierce ID, please email me at 
+	<a href="mailto:pmuller@fierceinc.com">pmuller@fierceinc.com</a>.</p><br/>
+	<p>We look forward to seeing you online.</p><br/>
+	<p>Pamela Muller<br/>
+	Fierce, Inc.<br/>
+	<a href="mailto:pmuller@fierceinc.com">pmuller@fierceinc.com</a></p>
+
+<br /><br />
+  </td>
+  <td width="24" background="http://www.fierceinc.com/uploads/images/special_email/special_right.gif">&nbsp;</td>
+</tr>
+
+<tr>
+  <td width="17" background="http://www.fierceinc.com/uploads/images/special_email/special_left.gif">&nbsp;</td>
+  <td width="557" style="color:#575657;font-family:arial,helvetica,sans-serif;font-size:8pt;line-height:12px;">
+
+<br />
+
+  </td>
+  <td width="24" background="http://www.fierceinc.com/uploads/images/special_email/special_right.gif">&nbsp;</td>
+</tr>
+<tr>
+  <td colspan="3"><img src="http://www.fierceinc.com/uploads/images/special_email/special_footer.gif" border="0" /></td>
+</tr>
+</table><br /><br />
+</div>
+</body>
+</html>
