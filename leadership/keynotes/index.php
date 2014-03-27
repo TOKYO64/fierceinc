139bacc9d8386025f4470ef78b38a8e2f9b56319
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 13:18:44 -0700

    Create index.php

---
+++ leadership/keynotes/index.php
@@ -0,0 +1,35 @@
+<? $pageTitle = 'Keynotes';
+include('../includes/head.php'); ?>
+
+		
+		<div id="feature" style="background: #5e727b url('../images/featKeynotes.jpg') no-repeat top right;">
+        
+		  <div id="featLt">
+				<img src="../images/featLtBookGr2.png" alt="Fierce Leadership" border="0" usemap="#Map" class="upper" />
+
+				<center><a href="<?php echo $URLROOT; ?>buy/"><img class="button" src="../images/butBuyBook.gif" /></a></center>
+		  </div>
+          
+          <div id="featVidlink">
+          	<a href="#" onclick="window.open('<?php echo $URLROOT; ?>video.html','video','width=615,height=480');return false;"><img src="../images/spacer.gif" alt=""></a>
+          </div>
+	
+            
+		</div>
+		
+		<div id="content">
+			<h1 class="colorPrimary">a special keynote opportunity<br />
+			with Susan Scott</h1>
+			<h2 class="colorSecondary">CEO and founder of Fierce, Inc.</h2>
+
+			<p>In her new book, <em>Fierce Leadership</em>, Scott busts six of the worst “best” business practices and reveals a technique she calls “squid eye”&mdash;the ability to see the obvious, once we know what to look for and to spot the “tells” that we’re falling prey to disastrous behaviors before they cripple us and our organizations. Scott offers surprising alternatives that leaders and managers at every level can put into place. With new approaches to everything from employee feedback, to corporate diversity, to customer relations, <em>Fierce Leadership</em> provides audiences with an honest look at what might be holding them back&mdash;and what to do about it.</p>
+			
+			<p>Scott is the founder of Fierce, Inc., whose diverse, global client list underscores a basic truth: <span class="bold">Business is fundamentally an extended conversation&mdash;with colleagues, customers and the unknown future emerging around us.</span> </p>
+			
+			<p>Whether you focus on implementing key initiatives, managing top talent, improving customer acquisition and retention, developing leaders, or simply igniting productive dialogue that generates clarity and impetus for change&mdash;success occurs one conversation at a time.</p>
+			
+			<p>To inquire about a keynote for your organization, <a href="http://www.fierceinc.com/index.php?page=contact_us&subject=Keynote Inquiry" target="FierceInc">contact</a> us.</p>
+
+		</div>
+		
+<? include('../includes/foot.php'); ?>
