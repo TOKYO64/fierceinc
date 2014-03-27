233edd7cda9821db0654dec21ea8edb0d1420b3e
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:49:48 -0700

    Create reviews.php

---
+++ leadership/book/reviews.php
@@ -0,0 +1,48 @@
+<? $pageTitle = 'Reviews | The Book';
+include('../includes/head.php'); ?>
+
+		
+		<div id="feature" style="background: #f5e5c7 url('../images/featBookC.png') no-repeat top left;">
+			<div id="featRt">
+				<img class="upper" src="../images/featRtRes2.png" alt="Free Resources" />
+				<center><a href="<?php echo $URLROOT; ?>resources/"><img class="button" src="../images/butResources.gif" /></a></center>
+			</div>
+		</div>
+		
+		<div id="sectionNav">
+			<ul>
+				<li id="first"><a href="<?php echo $URLROOT; ?>book/">Overview</a></li>
+				<li><a href="<?php echo $URLROOT; ?>book/excerpt.php">Excerpt</a></li>
+				<li class="active"><a href="<?php echo $URLROOT; ?>book/reviews.php">Reviews</a></li>
+			</ul>
+		</div>
+		
+		<div id="content">
+			<h1 class="colorPrimary">a few reviews</h1>
+			<br />
+			
+			<h4 class="colorSecondary"><em>“Business in today's environment is about change, which means it's about people and leadership. And it's about starting with the only thing you can really control as a leader, which is your own behavior. Susan Scott cuts through a lot of jargon and commonly accepted ideas and offers up specific, actionable steps to achieve better results. Pick one or two and get started today.”</em></h4>
+			
+			<p class="smcite">&mdash; BILL AYER, CHAIRMAN, PRESIDENT AND CEO, ALASKA AIR GROUP</p>
+			
+			<br />
+
+			
+			<div id="butBuyBook">
+				<a href="<?php echo $URLROOT; ?>buy/"><img src="../images/butBuyBook.gif" alt="BUY THE BOOK" /></a>
+			</div>
+			
+			<h4 class="colorSecondary"><em>“Susan Scott has successfully captured the potential shallowness and ineffectiveness of contemporary ‘best practices’ that often bedevil our business efforts.  Best practices may be ‘hot’ and ‘in style’ but may very well be counterproductive in leading your organization.”</em></h4>
+			
+			<p class="smcite">&mdash; JUAN GONZALEZ, VP STUDENT AFFAIRS, UNIVERSITY OF TEXAS, AUSTIN</p>
+			
+			<br />
+
+			
+			<h4 class="colorSecondary"><em>“</em>Fierce Leadership<em> is not for those who want to become shadows in the crowd; it is for those who are willing to tackle the biggest challenges in their lives, toss out the ‘old’ corporate way of thinking, and execute at a higher level of integrity, both personally and professionally.”</em></h4>
+			
+			<p class="smcite">&mdash; FAITH GREEN, CO-FOUNDER OF ARC - A RESOURCE COMMUNITY FOR WOMEN</p>
+
+		</div>
+		
+<? include('../includes/foot.php'); ?>
