a7b24b4580d8a726f153c2ed4ef56b70a93f6824
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 10:07:55 -0700

    Create qualifications.php

---
+++ authorizeprovider/facilitators/qualifications.php
@@ -0,0 +1,38 @@
+<?php 
+	$pageTitle = "Qualfications";
+	$tab = "facilitators";
+	include('../includes/head.php');
+?>
+
+	<div id="top_image">
+		<img src="<?php echo $URLROOT; ?>images/app_qualifications.jpg" width="780" height="214" />
+	</div>
+		
+	<div id="sectionNav">
+		<ul>
+			<li id="first"><a href="index.php">Overview</a></li>
+			<li class="active"><a href="qualifications.php">Qualifications</a></li>
+			<li><a href="process.php">The Process</a></li>
+		</ul>
+	</div>
+		
+	<div id="content">
+		<h1>Authorized Provider Trainer Qualifications</h1>
+		<p>To enroll in our Authorized Provider Train-the-Trainer Program, we ask that you meet the following criteria:</p>
+		<ul>
+			<li>You either work for, or are sponsored by, an approved or pre-existing Authorized Provider or Authorized Training 
+				Center.</li>
+			<li>You are a high-caliber facilitator who is experienced at delivering powerful and provocative training at all levels 
+				within an organization. People would pay to watch you in action.</li>
+			<li>You will internalize the content &mdash; connecting with the material intellectually and emotionally &mdash; so you 
+				can make it come alive for yourself and your audience.</li>
+			<li>Participation in a 2-Day Open Enrollment session prior to the 4-Day Train-the-Trainer event is recommended, but 
+				not required.</li>
+		</ul>
+		<p> </p>
+		<p>If you are a company or organization looking to bring Fierce programs in-house by having one of your employees certified as 
+			a trainer, please click <a href="http://www.fierceinc.com/index.php?page=certification" target="FierceInc">here</a> to learn more 
+			about our Enterprise Train-the-Trainer program.</p>
+	</div>
+		
+<?php include('../includes/foot.php'); ?>
