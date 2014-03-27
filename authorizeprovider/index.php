93e14f692840e22f0dd6a491ddf697d3a33674f4
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 10:09:35 -0700

    Create index.php

---
+++ authorizeprovider/index.php
@@ -0,0 +1,31 @@
+<?php 
+	$pageTitle = "Home";
+	$tab = "overview";
+	include('includes/head.php');
+?>
+
+	<div id="top_image">
+		<img src="images/app_home.jpg" width="780" height="214" />
+	</div>
+
+	<div id="home_powerpoint">
+		<a href="download"><img src="images/icon_powerpoint.gif" width="27" height="37" /></a>
+		<div><a href="download">DOWNLOAD POWERPOINT PRESENTATION</a></div>
+	</div>
+		
+	<div id="content">
+		<h1>Helping us to build a better world</h1>
+		<p>Our unabashed vision at Fierce is to &ldquo;build a better world, one conversation at a time.&rdquo; As part of that vision, we 
+			provide qualified and like-minded organizations with a mutually equitable opportunity to deliver Fierce programs to 
+			their own, unique audiences.</p>
+		<p>We call them Authorized Providers, and they are individuals or organizations that have met the necessary requirements 
+			to independently sell and deliver Fierce programs to their clientele. Authorized Providers may employ their own 
+			Certified Fierce Facilitator(s) to lead the sessions, or may hire one out from another cooperating Authorized Provider, 
+			Authorized Training Center, or directly from Fierce, when available.</p>
+		<p>To ensure your success as a provider and tour guide of Fierce, one person in your Authorized Provider organization 
+			must attend the Train-the-Trainer session; this person can be the same individual who also serves as your Certified 
+			Facilitator.</p>
+		<p>Click <a href="events/index.php">here</a> to view our upcoming Authorized Provider events.</p>
+	</div>
+		
+<?php include('includes/foot.php'); ?>
