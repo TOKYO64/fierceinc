40333772a418ab3e4cf384f256e7bed7a1913d5e
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 10:07:30 -0700

    Create process.php

---
+++ authorizeprovider/facilitators/process.php
@@ -0,0 +1,57 @@
+<?php 
+	$pageTitle = "Facilitators";
+	$tab = "facilitators";
+	include('../includes/head.php');
+?>
+
+	<div id="top_image">
+		<img src="<?php echo $URLROOT; ?>images/app_process.jpg" width="780" height="214" />
+	</div>
+		
+	<div id="sectionNav">
+		<ul>
+			<li id="first"><a href="index.php">Overview</a></li>
+			<li><a href="qualifications.php">Qualifications</a></li>
+			<li class="active"><a href="process.php">The Process</a></li>
+		</ul>
+	</div>
+		
+	<div id="content">
+		<h1>the process</h1>
+		<p>Becoming a certified Fierce Facilitator is no small thing. We believe that the skill with which our programs are 
+			delivered has everything to do with achieving results. Or not.</p>
+		<p>Therefore, we do not simply hand out certificates, slap you on the back, and hurry you out the door. Our process requires 
+			you to &ldquo;show up&rdquo;. Fully. And, quite often, your facilitation skills will noticeably improve as a direct result.</p>
+		<p>In order to facilitate any Fierce program, you must receive your certification in Fierce Conversations. Our foundational 
+			program. A must-have to become certifiably &ldquo;fierce&rdquo;.</p>
+		<p>Here is what you can expect.</p>
+		<br/>
+		<h2>preparation</h2>
+		<ul>
+			<li>Speak with a Fierce team member to enroll in the program.</li>
+			<li>Review the Fierce Train-the-Trainer Materials and know the content cold.</li>
+			<li>Read <a class="book" title="Visit the Fierce Conversations website" href="http://www.fierceconversations.com" target="Fierce"><em>Fierce Conversations &mdash; Achieving Success at Work &amp; in Life, One Conversation at a Time</em></a> by Susan Scott.</li>
+		</ul>
+		<br/>
+		<h2>certification class</h2>
+		<ul>
+			<li>Attend a 2 or 3-Day Train-the-Trainer Certification class led by a Master Facilitator. For large groups within a 
+				single organization, closed sessions may be arranged.</li>
+			<li>Deliver your Content assignments to the group throughout the event.</li>
+			<li>Actively participate by giving and receiving feedback.</li>
+			<li>Upon class completion, you will receive your Certification Level.</li>
+		</ul>
+		<br/>
+		<h2>next steps</h2>
+		<ul>
+			<li>If you have received our highest level of Certification (&ldquo;leaf&rdquo; level), you are free to begin training in all Fierce 
+				programs. Hooray!</li>
+			<li>If you have received a Certification Level below &ldquo;leaf&rdquo; level, a Master Facilitator will work with you to create 
+				a process that will bring you to full certification. Quite often, this involves co-facilitating with 
+				either a certified Fierce Facilitator or one of your internal trainers who has achieved &ldquo;leaf&rdquo; level.</li>
+		</ul>
+		<br/>
+		<p>Click <a href="<?php echo $URLROOT; ?>events">here</a> to view upcoming Authorized Provider Train-the-Trainer events.</p>
+	</div>
+		
+<?php include('../includes/foot.php'); ?>
