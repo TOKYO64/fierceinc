6447219c19278423159b6e7dfdab09e9cbca3eab
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:52:45 -0700

    Create index.php

---
+++ leadership/bureau/index.php
@@ -0,0 +1,45 @@
+<? $pageTitle = 'Speaker Bureaus';
+include('../includes/head.php'); ?>
+
+		
+		<div id="feature" style="background: #5e727b url('../images/featKeynotes.jpg') no-repeat top right;">
+        
+		  <div id="featLt">
+				<img src="../images/featLtBookGr2.png" alt="Fierce Leadership" border="0" usemap="#Map" class="upper" />
+
+				<center><a href="<?php echo $URLROOT; ?>buy/"><img class="button" src="../images/butBuyBook.gif" /></a></center>
+		  </div>
+          
+          <div id="featVidlink">
+          	<a href="#" onclick="window.open('../video.html','video','width=615,height=480');return false;"><img src="../images/spacer.gif" alt=""></a>
+          </div>
+	
+            
+		</div>
+		
+		<div id="content">
+			<h1 class="colorPrimary">welcome speaker bureaus!</h1>
+
+			<p>Susan Scott deeply values her friends and partners in the bureau channel. This page is designed to supply you with the resources you need to book one of America's top  speakers. Feel free to download the tools on this page for use on your website and with your clients. </p>
+		  <p>And if you need anything that you don’t see here, please call us at 206-787-1125 or email us at <a href="mailto:stacey@fierceinc.com">stacey@fierceinc.com</a></p><br />
+			
+            <h2 class="colorSecondary">Speaker Reel</h2>
+            <p> Download Susan's promotional video (12:24). <br />Available in .mpg (<a href="<?php echo $URLROOT; ?>bureau/SusanScott.mpg.zip">download here</a>) and .wmv (<a href="<?php echo $URLROOT; ?>bureau/SusanScott.wmv.zip">download here</a>)</p>
+
+
+          <br /><h2 class="colorSecondary">Sell Sheet</h2>
+            <p> Bureau version
+              <a href="<?php echo $URLROOT; ?>bureau/SusanScott_Bureau.pdf">download here.</a> Client-friendly version <a href="<?php echo $URLROOT; ?>bureau/SusanScott_OneSheet.pdf">download here.</a></p><br />
+            
+            <h2 class="colorSecondary">Biography</h2>
+            <p><a href="<?php echo $URLROOT; ?>bureau/SusanScott_Bio.pdf">Download here.</a></p>
+            
+        <br /><h2 class="colorSecondary">Headshot</h2>
+            <p> This high-resolution image can handle a multitude of uses.
+		<a href="<?php echo $URLROOT; ?>bureau/SusanScott_Headshot.jpg">Download here.</a></p><br />
+
+		<h2 class="colorSecondary">Fierce Conversations Also Available</h2>
+		<p>To learn more about Fierce Conversations Keynotes, go <a href="http://www.fierceconversations.com/keynotes" target="_blank">here</a>.</p>
+</div>
+		
+<? include('../includes/foot.php'); ?>
