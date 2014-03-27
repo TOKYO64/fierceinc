ee6d56e96e2b81394f0b7dbb049e8583c3321f5a
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 10:03:38 -0700

    Create thankyou.php

---
+++ authorizeprovider/apply/thankyou.php
@@ -0,0 +1,23 @@
+<?php 
+	$pageTitle = "Apply - Thank You";
+	$tab = "apply";
+	include('../includes/head.php');
+?>
+
+	<div id="apply-content">
+		<h2>Thank you</h2>
+		<div id="ap-apply">
+			<p>Thank you for submitting your application and for your interest in becoming a Fierce Authorized Provider. Our next 
+				step is to review your application which generally takes between 5 to 7 business days. We will then be in contact 
+				via email or phone to discuss the opportunity at length and to determine fit.</p>
+			<p>If you have any questions before that time, please call 206.787.1100 and ask for Traci Towle, although we would 
+				prefer to discuss program details once we've had an opportunity to thoroughly review your submission. You can 
+				find general information and resources about our Authorized Provider program on our website by clicking 
+				<a href="<?php echo $URLROOT; ?>">here</a>.</p>
+		</div>
+
+		<div style="clear: both;"></div>
+
+	</div>
+		
+<?php include('../includes/foot.php'); ?>
