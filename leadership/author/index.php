fab214a041332cf8ae40088b9dc155d349a3f36a
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:47:03 -0700

    Create index.php

---
+++ leadership/author/index.php
@@ -0,0 +1,27 @@
+<? $pageTitle = 'The Author';
+include('../includes/head.php'); ?>
+
+		
+		<div id="feature" style="background: #ea802d url('../images/featAuthor.jpg') no-repeat top right;">
+			<div id="featLt">
+				<img class="upper" src="../images/featLtBookOr.png" alt="Fierce Leadership" />
+				<center><a href="<?php echo $URLROOT; ?>buy/"><img class="button" src="../images/butBuyBook.gif" /></a></center>
+			</div>
+		</div>
+		
+		<div id="content">
+			<h1 class="colorPrimary">about Susan</h1>
+			
+			<p>Susan Scott is a best-selling author and leadership development architect who has enabled top executives worldwide to engage in vibrant dialogue with one another, with their employees, and with their customers for two decades. </p>
+			
+			<p>A recognized thought leader in the global business community and recipient of the 2008 Stevie award for Entrepreneur of the Year, Susan and her company are committed to large-scale and individual transformation through the principles set forth in Susan's books and her company's customized corporate training programs. </p>
+			
+			<p>For 13 years, Susan ran think tanks for CEOs and designed and delivered training to peers working with CEOs in 18 countries. In 2002, <em>Fierce Conversations—Achieving Success at Work and in Life, One Conversation at a Time</em>, was published in four countries and, shortly thereafter, was listed on The Wall Street Journal and UPI best seller lists, and was one of USA TODAY'S top 40 business books of 2002. Her much anticipated second book—<em>Fierce Leadership: An Alternative to the Worst “Best” Practices of Business Today</em> will be published in September 2009.</p>
+			
+			<p>Susan Scott is the founder of Fierce, Inc., a global training company whose clients include Yahoo, Starbucks, Cisco, BP, General Dynamics, New York Life, Four Seasons Hotels &amp; Resorts, C.A.R.E., Coca-Cola, Ernst &amp; Young. Susan lives in Seattle, Washington.</p>
+			
+			<p>For information about Susan’s keynotes, click <a href="<?php echo $URLROOT; ?>keynotes/">here</a>.</p>
+
+		</div>
+		
+<? include('../includes/foot.php'); ?>
