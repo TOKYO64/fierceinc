c0d04d9b4a69eb315b1abefcf408a03b393c3448
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:53:21 -0700

    Create index.php

---
+++ leadership/buy/index.php
@@ -0,0 +1,23 @@
+<? $pageTitle = 'Buy the Book';
+include('../includes/head.php'); ?>
+
+		
+		<div id="feature" style="background: #e2e4bb url('../images/featBuy.gif') no-repeat top right;">
+			<img src="../images/featBuy.gif" alt="Start practicing Fierce Leadership today." usemap="#fbuy" />
+			<map name="fbuy">
+				<area target="_blank" href="http://www.amazon.com/gp/product/038552904X?ie=UTF8&tag=fierceconvers-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=038552904X%22%3EFierce%20Leadership:%20A%20Bold%20Alternative%20to%20the%20Worst%20%22Best%22%20Practices%20of%20Business%20Today" alt="Order from Amazon.com" shape="rect" coords="165,112,294,146" />
+				<area target="_blank" href="http://www.borders.com/online/store/TitleDetail?type=1&catalogId=10001&simple=1&defaultSearchView=List&keyword=Fierce+Leadership+paperback&LogData=[search%3A+238%2Cparse%3A+255]&searchData={productId%3Anull%2Csku%3Anull%2Ctype%3A1%2Csort%3Anull%2CcurrPage%3A1%2CresultsPerPage%3A25%2CsimpleSearch%3Atrue%2Cnavigation%3A5185%2CmoreValue%3Anull%2CcoverView%3Afalse%2Curl%3Arpp%3D25%26view%3D2%26type%3D1%26nav%3D5185%26simple%3Dtrue%26book_search%3DFierce%2BLeadership%2Bpaperback%2Cterms%3A{book_search%3DFierce+Leadership+paperback}}&storeId=13551&sku=038552904X&ddkey=http:SearchResults" alt="Order from Borders.com" shape="rect" coords="165,157,294,191" />
+				<area target="_blank" href="http://search.barnesandnoble.com/Fierce-Leadership/Susan-Scott/e/9780385529044/?itm=1&USRI=fierce+leadership+paperback" alt="Order from BN.com" shape="rect" coords="165,202,294,236" />
+				<area target="_blank" href="http://800ceoread.com/book/show/9780385529044-Fierce_Leadership" alt="Order from 800ceoread.com" shape="rect" coords="165,247,294,281" />
+			</map>
+		</div>
+		
+		<br />
+		
+		<!-- oustide #content because of width -->
+		<a href="http://www.fierceinc.com/conversations/buy" target="_blank"><img src="../images/fcBuy.png" alt="" id="buyfc" usemap="#buyfc" /></a>
+		<map name="buyfc">
+			<area href="" alt="BUY" shape="rect" coords="345,140,388,157" />
+		</map>
+		
+<? include('../includes/foot.php'); ?>
