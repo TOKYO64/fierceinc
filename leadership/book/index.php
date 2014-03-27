1983a280a16707113c228a01efe1a339bbc593dc
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:49:24 -0700

    Create index.php

---
+++ leadership/book/index.php
@@ -0,0 +1,70 @@
+<?php $pageTitle = 'The Book';
+include('../includes/head.php'); ?>
+
+		
+		<div id="feature" style="background: #e6e6e6 url('../images/featBookA.png') no-repeat top left;">
+			<div id="featRt">
+				<img class="upper" src="../images/featRtRes2.png" alt="Free Resources" />
+				<center><a href="<?php echo $URLROOT; ?>resources/"><img class="button" src="../images/butResources.gif" /></a></center>
+			</div>
+		</div>
+		
+		<div id="sectionNav">
+			<ul>
+				<li class="active" id="first"><a href="<?php echo $URLROOT; ?>book/">Overview</a></li>
+				<li><a href="<?php echo $URLROOT; ?>book/excerpt.php">Excerpt</a></li>
+				<li><a href="<?php echo $URLROOT; ?>book/reviews.php">Reviews</a></li>
+			</ul>
+		</div>
+		
+		<div id="content">
+			<h1 class="colorPrimary">redefining leadership</h1>
+			
+			<br />
+			<img src="../images/fierceLeadPron.png" alt="[pronunciation]" />
+			<br />
+			<br />
+			
+			<ol class="colorSecondary serif larger">
+				<li><span class="sans colorText regular">a fast-acting anti-venom to the business-as-usual mode of high task/low relationship, self-serving agendas, directing and telling, anonymous feedback, holding people accountable, excessive use of jargon and hiring smart people who lack heart.</span></li>
+				<li><span class="sans colorText regular">the act of acquiring your most valuable currency&mdash;emotional capital.</span></li>
+				<li><span class="sans colorText regular">also signals the demise of truth-telling squeamishness and ethical squishiness.</span></li>
+			</ol>
+			
+			<br />
+			<br />
+			
+			<div id="butBuyBook">
+				<a href="<?php echo $URLROOT; ?>buy/"><img src="../images/butBuyBook.gif" alt="BUY THE BOOK" /></a>
+			</div>
+			
+			<h2 class="colorPrimary">with <em>Fierce Leadership</em> you’ll discover how to</h2>
+			
+			<ul class="colorSecondary annlam">
+				<li><span class="colorText">Recognize and counteract the worst “best” practices of leaders today.</span></li>
+				<li><span class="colorText">Gain the new sustainable, competitive edge&mdash;human connectivity&mdash;the next frontier for exponential growth.</span></li>
+				<li><span class="colorText">Develop “squid eye” &mdash; and spot the “tells” that signal imminent trouble around your conference table, your organization or your team.</span></li>
+			</ul>
+			
+			<br />
+			
+			<p>In <span class="em">Fierce Leadership</span>, Scott teaches us how to spot the worst “best” practices in our organizations using a technique she calls “squid eye”&mdash;the ability to see the “tells” or signs that we have fallen prey to disastrous behaviors  by knowing what to look for. Only then, she says, can we apply the antidote...</p>
+
+			<p>Informed by over a decade of conversations with Fortune 500 executives, this book is that antidote. With fierce new approaches to everything from employee feedback to corporate diversity to customer relations, Scott offers fresh and surprising alternatives to six of the so-called “best” practices permeating today’s businesses. This refreshingly candid book is a must-read for any manager or leader at any level who is ready to take a long hard look at what trouble might be lurking in their organization&mdash;and do something about it.</p>
+			
+			<br />
+
+			<h2 class="colorPrimary">redefining the “best” practices</h2>
+			
+			<ul class="colorSecondary larger" style="list-style: none; text-indent: -20px;">
+				<li>1 <span class="serif colorText regular">From 360 Anonymous Feedback &mdash; to 365 Face-to-Face Feedback</span></li>
+				<li>2 <span class="serif colorText regular">From Hiring for Smarts &mdash; to Hiring for Smart+Heart</span></li>
+				<li>3 <span class="serif colorText regular">From Holding People Accountable &mdash; to Modeling Accountability and Holding People Able</span></li>
+				<li>4 <span class="serif colorText regular">From Employee Engagement Programs &mdash; to Real Engagement</span></li>
+				<li>5 <span class="serif colorText regular">From Client Centricity &mdash; to Client Connectivity</span></li>
+				<li>6 <span class="serif colorText regular">From Legislated Optimism &mdash; to Radical Transparency</span></li>
+			</ul>
+
+		</div>
+		
+<? include('../includes/foot.php'); ?>
