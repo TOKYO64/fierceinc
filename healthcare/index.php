67dd38244d9dd1bf371b241126b7e7f5841d4536
Tokyo64 <tonyberry@tokyo64.com>
2014-03-27 12:41:37 -0700

    Create index.php

---
+++ healthcare/index.php
@@ -0,0 +1,63 @@
+<? set_include_path(get_include_path().':'.$_SERVER['DOCUMENT_ROOT']  .':'.'/var/www/html/miller/SusanScott/ht/'  );
+$pageTitle = 'Health Care';
+include('includes/headalt.php'); ?>
+        <a href="http://www.fierceinc.com/index.php?page=fierce_conversations" target="FierceInc"><img src="/healthcareimages/bigTraining2.png" alt="Coming Soon. Fierce Leadership Workshops." style="margin-left: 21px;" usemap="#trn1" /></a>
+        
+<br />
+		<br />
+		<map name="trn1">
+			<area href="http://www.fierceinc.com/index.php?page=fierce_conversations" target="FierceInc" alt="click here" shape="rect" coords="435,270,463,284" />
+		</map>
+		
+		
+		<div id="content">
+			<h2 class="colorPrimary">Research shows that better conversations reduce malpractice risk</h2>
+			
+		  <p>At Fierce, we believe that a single conversation can have far-reaching impact. This premise holds true in the world of healthcare, as indicated by studies such as one conducted by Dr. Wendy Levinson, Chair of the Department of Medicine at the University of Toronto. Her study, <i>Physician-patient communication: The relationship with malpractice claims among primary care physicians and surgeons</i> (JAMA, Vol. 277 No. 7, February 19, 1997) examined hundreds of conversations between a group of physicians and their patients. Those physicians who spent more quality time with their patients were not sued for malpractice. The tone and content of those discussions were what mattered most.<br /><br />
+		    A July 2008 report issued by Press Ganey,<i> Reduce Liability Risk by Improving Your Patient Satisfaction,</i> similarly shows that patient dissatisfaction—contributed to by factors such as poor communication and lack of disclosure -- is the most accurate indicator of whether a patient will sue their physician. This issue is of such recent interest that the June 2009 New York Times article <i>If All Doctors Had More Time to Listen</i> discusses how some doctors are attempting to reinvent healthcare through "patient-centered practices" that allow them to emphasize prevention and education. Patients in such practices are more likely to say they received the care they needed.<br /><br />
+		    The idea that better patient conversations can lead to improved relationships has been discussed for more than a decade. So how does one learn the mindset and skill set that's needed to have conversations that will make us all healthier?  <br /><br />
+		    Fierce's methodology revolves around the very idea that the conversation is the relationship, and the relationship impacts the results. Based on the principles of Susan Scott's best-seller, <i>Fierce Conversations: Achieving Success in Work &amp; in Life, One Conversation at a Time,</i> this foundational, hands-on workshop will introduce you to the transformational ideas and principles that will shift your basic understanding of conversations and the power they hold in relationships, leadership, and results.  <br /><br />
+		    We have helped healthcare leaders such as the University of Washington School of Medicine, Harborview Medical Center and Group Health Cooperative transform the conversations central to their success.  <br /><br />
+	      To learn more about Fierce training programs <a href="http://www.fierceinc.com/index.php?page=contact_us" target="FierceInc">click here</a>.</p>
+<br /><br />
+		  <h2 class="colorPrimary">testimonials</h2>
+          
+          <div id="butBuyBook">
+                
+				<a href="http://www.amazon.com/gp/product/0425193373?ie=UTF8&tag=fierceconvers-20&linkCode=as2&camp=1789&creative=390957&creativeASIN=0425193373" target="blank"><img src="/healthcareimages/health_bug3.jpg" alt="BUY THE BOOK" /></a>
+               
+			</div>
+
+
+			<h4 class="colorSecondary"><i>"I see Fierce Conversations as a vital tool for healthcare professionals, as so many of the conversations we have every day will only be successful if handled with the authenticity and candor that Fierce teaches. For example, the techniques can help nurses and physicans talk with patients about compliance issues; assist nurse preceptors in being better mentors to their nurses-in-training; help nurse managers talk more powerfully with physicians; and provide faculty with a structure for powerful teaching conversations.  The delegation module will also be useful for nurse managers and new nurses in their daily work."</i> </h4>
+		  <p>- KATHLEEN PHILLIPS, FORMER ASST. DEAN OF NURSING<br />
+		    LOYOLA UNIVERSITY
+</p><br />
+
+		<h4 class="colorSecondary"><i>"Fierce Conversations is the core training for the Communications portion of our Leadership Development program. It has given participants the necessary tools to have conversations—individually and as teams—that address and resolve performance and behavioral issues, while building and strengthening relationships."</i></h4>
+			<p>- BILL CROWDER, REGISTRED CORPORATE COACH<br />
+			  ANTHEM BLUE CROSS & BLUE SHIELD
+
+</p><br />
+
+		  <h2 class="colorPrimary">a partial list of our healthcare clients</h2>
+<p>	American Academy of Otolaryngology<br />
+	American Dietetic Association	<br />
+	Anthem Blue Cross & Blue Shield<br />
+	Calgary Laboratory Services<br />
+	Eli Lilly and Company<br />
+	GlaxoSmithKline<br />
+	Group Health Cooperative<br />
+	National Instittute of Health<br />
+	University of Washington School of Medicine<br />
+	University of Washington School of Nursing<br /><br />
+    
+  <div id="butBuyBook3">
+			</div>
+
+
+Visit the Fierce, Inc. <a href="http://www.fierceinc.com" target="FierceInc">website here.</a></div>
+        
+
+		
+<? include('includes/foot.php'); ?>
